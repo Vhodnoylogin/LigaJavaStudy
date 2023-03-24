240f5c6f695e8d1c5b79f8bb5d95afadd9d5572e
Vhodnoylogin <kill.spama@gmail.com>
2023-03-25 01:46:40 +0300

    доделал контроллер параметров

--- src/main/java/ru/liga/karmatskiyrg/controller/ParameterController.java
+++ src/main/java/ru/liga/karmatskiyrg/controller/ParameterController.java
@@ -4,21 +4,25 @@ import ru.liga.karmatskiyrg.model.currency.CurrencyRate;
 import ru.liga.karmatskiyrg.model.dicts.interfaces.DCurrencyType;
 import ru.liga.karmatskiyrg.service.interfaces.CurrencyPredict;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ParameterController {
 
-    protected final CurrencyPredict prediction;
+    private final CurrencyPredict prediction;
 
     public ParameterController(CurrencyPredict prediction) {
         this.prediction = prediction;
     }
 
     public List<CurrencyRate> getCurrencyRateTomorrow(DCurrencyType type) {
-        throw new RuntimeException();
+        return this.prediction.predictToDate(type, LocalDate.now().plusDays(1))
+                .stream()
+                .limit(1)
+                .toList();
     }
 
     public List<CurrencyRate> getCurrencyRateWeek(DCurrencyType type) {
-        throw new RuntimeException();
+        return this.prediction.predictToDate(type, LocalDate.now().plusDays(8));
     }
 }
