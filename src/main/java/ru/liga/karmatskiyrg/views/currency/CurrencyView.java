6f3bacf574ec3d9e8ee7d8e64e134133dc3fb587
Vhodnoylogin <kill.spama@gmail.com>
2023-03-24 22:56:06 +0300

    rename view

--- src/main/java/ru/liga/karmatskiyrg/views/currency/CurrencyView.java
+++ src/main/java/ru/liga/karmatskiyrg/views/currency/CurrencyView.java
@@ -1,17 +1,15 @@
 package ru.liga.karmatskiyrg.views.currency;
 
-import org.jetbrains.annotations.NotNull;
+import lombok.Value;
 import ru.liga.karmatskiyrg.model.currency.CurrencyRate;
 import ru.liga.karmatskiyrg.views.interfaces.View;
 
 import java.util.List;
 
-public class VCurrencyToConsole implements View {
-    protected final List<CurrencyRate> results;
+@Value
+public class CurrencyView implements View {
+    private final List<CurrencyRate> results;
 
-    public VCurrencyToConsole(@NotNull List<CurrencyRate> results) {
-        this.results = results;
-    }
 
     @Override
     public void show() {
