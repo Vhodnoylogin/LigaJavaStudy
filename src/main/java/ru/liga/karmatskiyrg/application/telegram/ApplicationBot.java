08be10610307bcf69183cb43f2ab5fa6c9660de2
Vhodnoylogin <kill.spama@gmail.com>
2023-04-04 00:51:33 +0300

    оно живое и даже что-то показывает

--- src/main/java/ru/liga/karmatskiyrg/application/telegram/ApplicationBot.java
+++ src/main/java/ru/liga/karmatskiyrg/application/telegram/ApplicationBot.java
@@ -1,64 +1,29 @@
 package ru.liga.karmatskiyrg.application.telegram;
 
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.liga.karmatskiyrg.controller.errors.NotValidCommand;
-import ru.liga.karmatskiyrg.controller.loop.ParameterController;
-import ru.liga.karmatskiyrg.controller.observers.ParameterLeadAction;
-import ru.liga.karmatskiyrg.controller.observers.dicts.IsCommandString;
-import ru.liga.karmatskiyrg.controller.observers.initialize.Init;
-import ru.liga.karmatskiyrg.model.context.loop.RateLoopContext;
-import ru.liga.karmatskiyrg.model.dicts.commands.interfaces.DCommand;
-import ru.liga.karmatskiyrg.model.dicts.dates.DDateTypes;
-import ru.liga.karmatskiyrg.repository.CurrencyRepoRAM;
-import ru.liga.karmatskiyrg.service.currency.CsvToCurrency;
-import ru.liga.karmatskiyrg.service.currency.PredictCurrencyRateOld;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.liga.karmatskiyrg.service.telegram.bot.TelegramBot;
-import ru.liga.karmatskiyrg.utils.csv.CsvFileLayout;
-
-import java.util.List;
 
 public class ApplicationBot extends TelegramBot {
     private static final String BOT_NAME = "Vhodnoylogin_tst_bot";
     private static final String BOT_TOKEN = "6130241804:AAH0icUms3upX96vuFUl8eWQtwBelCM7BsM";
 
-//    private final TelegramRateContext context = new TelegramRateContext();
+    private final InnerApp app;
 
     public ApplicationBot() {
         super(BOT_NAME, BOT_TOKEN);
-        Init.initDictionaries();
-
+        app = new InnerApp();
+        app.initCommands();
     }
 
     @Override
     public void processNonCommandUpdate(Update update) {
-        Message msg = update.getMessage();
-        Long chatId = msg.getChatId();
-//        String userName = getUserName(msg);
-
-        var tokens = msg.getText();
+        var send = app.content(update);
+        try {
+            execute(send);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void initParameters(RateLoopContext context) {
-        var leadParameter = ParameterLeadAction.getSingleton();
-
-        var db = new CurrencyRepoRAM();
-        db.save(CsvToCurrency.getCurrencyRate(CsvFileLayout.csvFile));
-        var predication = new PredictCurrencyRateOld(db);
-
-        var controller = new ParameterController(context, predication);
-        leadParameter.addVariant(DDateTypes.TMR, controller::getCurrencyRateTomorrow);
-        leadParameter.addVariant(DDateTypes.WEK, controller::getCurrencyRateWeek);
-    }
-
-    private DCommand parseTokens(List<String> tokens) {
-        if (tokens.isEmpty())
-            throw new NotValidCommand("Invalid number of arguments");
-
-        var command = IsCommandString.getSingleton().getFirstVariant(tokens.get(0));
-        if (command == null)
-            throw new NotValidCommand(String.format("Command %s not found", tokens.get(0)));
-
-        return command;
-    }
 }
